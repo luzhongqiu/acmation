6b87ee085c9574dd2d6150ff778d5583892ff7d4
Lu ZhongQiu <zhongqiu.lu@patsnap-qa.local>
2018-04-24 13:13:40 +0800

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='acmation',
+    version='v0.1',
+    packages=find_packages,
+    url='http://git.patsnap.com/zhongqiu.lu/acmation',
+    license='mit',
+    author='zq',
+    author_email='luzhongqiu@patsnap.com',
+    description='ac automation'
+)
